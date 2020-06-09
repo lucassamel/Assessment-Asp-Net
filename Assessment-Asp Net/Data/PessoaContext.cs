09f3ef8307cba0f8f9d9e63203f68437c25d39cd
Lucas Samel <lucas.samel97@gmail.com>
2020-06-09 17:39:21 -0300

    Add project files.

---
+++ Assessment-Asp Net/Data/PessoaContext.cs
@@ -0,0 +1,18 @@
+﻿using Assessment_Asp_Net.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assessment_Asp_Net.Data
+{
+    public class PessoaContext : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public PessoaContext(DbContextOptions<PessoaContext> options) : base(options)
+        {
+
+        }
+    }
+}
